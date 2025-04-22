b7a8dd05f4e35163f7f32c014c9ccabd8b2e2e30
Violetta Vinakova <vinakova.vita@gmail.com>
2025-04-22 18:04:01 +0300

    feat: add JSON fixtures and change test cases

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,141 +1,37 @@
-import { parseFile } from '../src/parseFile.js';
-import buildDiff from '../src/buildDiff.js';
+import genDiff from '../index.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
 
-describe('parseFile', () => {
-  test('parses JSON', () => {
-    const jsonString = '{"key": "value"}';
-    const expected = { key: 'value' };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    expect(parseFile(jsonString, '.json')).toEqual(expected);
-  });
+const getFixtures = filename => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = filename => fs.readFileSync(getFixtures(filename), 'utf-8');
 
-  test('parses YAML', () => {
-    const yamlString = 'key: value';
-    const expected = { key: 'value' };
+test('JSON format', () => {
+  const file1 = getFixtures('f1.json');
+  const file2 = getFixtures('f2.json');
 
-    expect(parseFile(yamlString, '.yml')).toEqual(expected);
-  });
+  const expected = readFile('resultJSON.txt');
 
-  test('parses YAML with .yaml extension', () => {
-    const yamlString = 'key: value';
-    const expected = { key: 'value' };
-
-    expect(parseFile(yamlString, '.yaml')).toEqual(expected);
-  });
-
-  test('throws an error for unsupported formats', () => {
-    expect(() => parseFile('some data', '.txt')).toThrow('Unsupported format: .txt');
-  });
-
-  test('throws an error for invalid JSON', () => {
-    const invalidJson = '{ key: value }';
-    expect(() => parseFile(invalidJson, '.json')).toThrow();
-  });
-
-  test('throws an error for invalid YAML', () => {
-    const invalidYaml = ': invalid : : :';
-    expect(() => parseFile(invalidYaml, '.yml')).toThrow();
-  });
+  expect(genDiff(file1, file2)).toBe(expected);
 });
 
-describe('buildDiff', () => {
-  test('returns diff for flat objects', () => {
-    const obj1 = { key1: 'value1', key2: 'value2' };
-    const obj2 = { key2: 'value2', key3: 'value3' };
+test('Plain format', () => {
+  const file1 = getFixtures('f1.json');
+  const file2 = getFixtures('f2.json');
 
-    const expected = [
-      { key: 'key1', status: 'removed', value: 'value1' },
-      { key: 'key2', status: 'unchanged', value: 'value2' },
-      { key: 'key3', status: 'added', value: 'value3' },
-    ];
+  const expected = readFile('resultPlain.txt');
 
-    expect(buildDiff(obj1, obj2)).toEqual(expected);
-  });
+  expect(genDiff(file1, file2, 'plain')).toBe(expected);
+});
 
-  test('handles identical objects', () => {
-    const obj1 = { key1: 'value1', key2: 'value2' };
-    const obj2 = { key1: 'value1', key2: 'value2' };
+test('Stylish format', () => {
+  const file1 = getFixtures('f1.json');
+  const file2 = getFixtures('f2.json');
 
-    const expected = [
-      { key: 'key1', status: 'unchanged', value: 'value1' },
-      { key: 'key2', status: 'unchanged', value: 'value2' },
-    ];
-    expect(buildDiff(obj1, obj2)).toEqual(expected);
-  });
+  const expected = readFile('resultStylish.txt');
 
-  test('handles empty objects', () => {
-    const obj1 = {};
-    const obj2 = {};
-
-    const expected = [];
-
-    expect(buildDiff(obj1, obj2)).toEqual(expected);
-  });
-
-  test('handles added keys', () => {
-    const obj1 = {};
-    const obj2 = { key1: 'value1' };
-
-    const expected = [
-      { key: 'key1', status: 'added', value: 'value1' },
-    ];
-
-    expect(buildDiff(obj1, obj2)).toEqual(expected);
-  });
-
-  test('handles removed keys', () => {
-    const obj1 = { key1: 'value1' };
-    const obj2 = {};
-
-    const expected = [
-      { key: 'key1', status: 'removed', value: 'value1' },
-    ];
-
-    expect(buildDiff(obj1, obj2)).toEqual(expected);
-  });
-
-  test('handles changed values', () => {
-    const obj1 = { key1: 'value1' };
-    const obj2 = { key1: 'value2' };
-
-    const expected = [
-      { key: 'key1', status: 'changed', oldValue: 'value1', value: 'value2' },
-    ];
-
-    expect(buildDiff(obj1, obj2)).toEqual(expected);
-  });
-
-  test('handle nested objects', () => {
-    const obj1 = {
-      name: 'Veta', pets: [
-        { type: 'cat', name: 'Lilo' },
-        { type: 'dog', name: 'Rex' },
-      ],
-    };
-    const obj2 = {
-      name: 'Veta', pets: [
-        { type: 'rat', name: 'Lilo' },
-        { type: 'cat' },
-      ],
-    };
-
-    const expected = [
-      { key: 'name', value: 'Veta', status: 'unchanged' },
-      {
-        key: 'pets',
-        status: 'nested-array',
-        value: [
-          { key: '0', status: 'nested-object', value: [
-            { key: 'type', oldValue: 'cat', value: 'rat', status: 'changed' },
-            { key: 'name', value: 'Lilo', status: 'unchanged' },
-          ] },
-          { key: '1', status: 'nested-object', value: [
-            { key: 'type', oldValue: 'dog', value: 'cat', status: 'changed' },
-            { key: 'name', value: 'Rex', status: 'removed' },
-          ] },
-        ],
-      },
-    ];
-    expect(buildDiff(obj1, obj2)).toEqual(expected);
-  });
+  expect(genDiff(file1, file2, 'stylish')).toBe(expected);
 });
